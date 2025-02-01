3bcb36ce2fc1ec037bcc048244b455d51f3cab30
albertoarr <albertoarranzromano@gmail.com>
2025-02-01 15:25:49 +0000

    server arreglado

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,27 @@
-const express = require('express');
-const { Pool } = require('pg');  // Importamos el paquete pg
-const cors = require('cors');
+const express = require("express");
+const postgres = require("postgres"); // Importamos el paquete pg
+const cors = require("cors");
 
 const app = express();
 const port = 3002;
 app.use(cors());
 
 // Configuración de la conexión a PostgreSQL
-const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',        // Usuario de PostgreSQL (ajústalo si es necesario)
-  password: '5206',            // Contraseña de tu base de datos PostgreSQL
-  database: 'colegio',      // Nombre de la base de datos
-  port: 5432               // Puerto por defecto de PostgreSQL
-});
-
-// Verificar la conexión a la base de datos
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Error al conectar a la base de datos', err.stack);
-  }
-  console.log('DB Connected!');
-  release();  // Liberamos el cliente después de la conexión
+const connection = postgres({
+  host: "localhost",
+  user: "postgres",
+  password: "5206",
+  database: "colegio",
 });
 
 // Ruta para obtener los productos
-app.get('/api/productos', async (req, res) => {
+app.get("/api/alumnos", async (request, response) => {
   try {
-    const sql_query = 'SELECT * FROM productos';
-    const result = await pool.query(sql_query);  // Usamos pool.query para ejecutar la consulta
-
-    console.log("Resultado:", result.rows);
-    res.json(result.rows);  // Enviamos solo las filas del resultado
-  } catch (err) {
-    console.error('Error ejecutando la consulta', err.stack);
-    res.status(500).send('Error en el servidor');
+    const result = await connection`SELECT * FROM alumnos`;
+    response.json(result);
+  } catch (e) {
+    console.error("Error en la consulta:", e);
+    response.status(500).json({ error: "Error en la base de datos " });
   }
 });
 
