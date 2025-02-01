6a2047dd960479c5ed1335998ac37513e3c5ee75
albertoarr <albertoarranzromano@gmail.com>
2025-02-01 12:55:23 +0000

    casa

--- frontendInformes/src/pages/Home.tsx
+++ frontendInformes/src/pages/Home.tsx
@@ -1,22 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonText, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonText>Hola putita</IonText>
       </IonContent>
     </IonPage>
   );
