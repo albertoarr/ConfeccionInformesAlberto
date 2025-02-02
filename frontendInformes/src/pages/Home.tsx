718cc228ac4b96ba896f0162f252b957b1c31829
albertoarr <albertoarranzromano@gmail.com>
2025-02-02 19:46:57 +0000

    casa

--- frontendInformes/src/pages/Home.tsx
+++ frontendInformes/src/pages/Home.tsx
@@ -21,6 +21,27 @@ export default function Home() {
             <p>hola</p>
           </div>
         </div>
+        <div
+          className="d-flex justify-content-center align-items-center bg-secondary"
+          style={{ height: "100vh" }}
+        >
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <div className="p-5 bg-light border rounded">
+                  <h2>Contenedor 1</h2>
+                  <p>Contenido dentro del contenedor 1.</p>
+                </div>
+              </div>
+              <div className="col-12 mt-3">
+                <div className="p-5 bg-light border rounded">
+                  <h2>Contenedor 2</h2>
+                  <p>Contenido dentro del contenedor 2.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
